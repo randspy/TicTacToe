a0b7ae93ad581c5345176d8b41188aa6dda4e333
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-06-04 20:07:49 +0200

    The most basic call in Main.

--- src/main/com/randspy/tictactoe/Main.java
+++ src/main/com/randspy/tictactoe/Main.java
@@ -1,7 +1,18 @@
 package com.randspy.tictactoe;
 
+import com.randspy.tictactoe.logic.Game;
+import com.randspy.tictactoe.logic.HumanPlayer;
+import com.randspy.tictactoe.logic.Player;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Game game = new Game();
+        Player humanPlayer = new HumanPlayer();
+
+        while (!game.isFinished()) {
+            game.play(humanPlayer, new Integer(System.console().readLine()));
+        }
     }
 }
