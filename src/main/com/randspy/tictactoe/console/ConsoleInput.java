16aa9bca9e83c12f74adcecd57f2599cc27636ba
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-06-05 20:52:25 +0200

    Offset added. From now on left top corner will start with 1. There is something wrong with this solution. Refactor in future.

--- src/main/com/randspy/tictactoe/console/ConsoleInput.java
+++ src/main/com/randspy/tictactoe/console/ConsoleInput.java
@@ -3,8 +3,10 @@ package com.randspy.tictactoe.console;
 import com.randspy.tictactoe.logic.HumanInput;
 
 public class ConsoleInput implements HumanInput {
+    private Integer offset = 1;
+
     @Override
     public int getInput() {
-        return new Integer(System.console().readLine());
+        return new Integer(System.console().readLine()) - offset;
     }
 }
