53f492328e875d0a20b38b6a781576f709b07d26
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-06-27 11:49:51 +0200

    Tests for ComputerPlayer added.

---
+++ src/test/com/randspy/tictactoe/logic/ComputerPlayerTest.java
@@ -0,0 +1,47 @@
+package com.randspy.tictactoe.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+public class ComputerPlayerTest {
+
+    private ComputerPlayer computer;
+    private PlayerId opponent;
+    private Board board;
+
+    private void expectMove(PositionOnBoard opponentsMove, int expectedRow, int expectedColumn) {
+        board.setPlayerAtPosition(opponent, opponentsMove);
+        PositionOnBoard positionOnBoard = computer.nextMove(board);
+        board.setPlayerAtPosition(computer.getId(), positionOnBoard);
+        assertEquals(expectedRow, positionOnBoard.getRow());
+        assertEquals(expectedColumn, positionOnBoard.getColumn());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        computer = new ComputerPlayer();
+        opponent = new PlayerId(UUID.randomUUID());
+        board = new Board();
+    }
+
+    @Test
+    public void computerWinningGame() {
+        expectMove(new PositionOnBoard(1, 1), 0, 0);
+        expectMove(new PositionOnBoard(2, 2), 0, 2);
+        expectMove(new PositionOnBoard(2, 1), 0, 1);
+    }
+
+
+    @Test
+    public void tieGame() {
+        expectMove(new PositionOnBoard(1, 1), 0, 0);
+        expectMove(new PositionOnBoard(0, 2), 2, 0);
+        expectMove(new PositionOnBoard(1, 0), 1, 2);
+        expectMove(new PositionOnBoard(2, 1), 0, 1);
+    }
+
+}
